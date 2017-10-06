eeca26a0ead40a0f8a4ec35f5a27ae8a1fa4c9ed
tawright915 <tawright915@gmail.com>
2017-10-05 20:00:13 -0500

    Update made to js and html

--- bg.js
+++ bg.js
@@ -1,1 +1,8 @@
-alert('hello world');
+var search = document.getElementById("searchQuery").innerText;
+if (search != "")
+{
+    window.location.href("https://www.google.com/search?safe=active&source=hp&q=" + search);
+}
+else{
+    alert("Kinda hard for search for nothin");
+}
