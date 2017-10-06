90b0cbb22376e31280f98454dbb2874b04e60f1b
tawright915 <tawright915@gmail.com>
2017-10-06 15:56:30 -0500

    Update to Most recent branch

--- bg.js
+++ bg.js
@@ -1,8 +1,13 @@
-var search = document.getElementById("searchQuery").innerText;
-if (search != "")
-{
-    window.location.href("https://www.google.com/search?safe=active&source=hp&q=" + search);
-}
-else{
-    alert("Kinda hard for search for nothin");
+function mySearch(){
+    
+    var search = document.getElementById("searchQ").innerText;
+    alert(search);
+    if (search != "")
+    {
+        window.location.href("https://www.google.com/search?safe=active&source=hp&q=" + search);
+    }
+    else{
+        alert("Kinda hard for search for nothin");
+    }
+
 }
